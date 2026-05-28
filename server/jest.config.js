ff0e4164146f75610cd303ff1042a9611ad24763
linhgrr <linh.nbt225732@sis.hust.edu.vn>
2026-05-28 08:57:21 +0700

    fix(server): correct Jest setup option (setupFilesAfterEnv) and remove unknown keys

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,9 +1,7 @@
 export default {
   testEnvironment: 'node',
-  setupFilesAfterEach: [],
-  globalSetup: undefined,
   testMatch: ['<rootDir>/tests/**/*.test.js'],
-  setupFilesAfterEach: ['<rootDir>/tests/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   transform: {},
   testTimeout: 30_000,
   verbose: true,
